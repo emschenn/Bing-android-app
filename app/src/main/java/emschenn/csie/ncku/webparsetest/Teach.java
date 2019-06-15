9e30b3948af4ca870aaf30a80ee6e08fc55ec5bb
di-yuan <big9917@gmail.com>
2019-06-16 01:58:28 +0800

    didi add teach slider

--- app/src/main/java/emschenn/csie/ncku/webparsetest/Teach.java
+++ app/src/main/java/emschenn/csie/ncku/webparsetest/Teach.java
@@ -1,13 +1,21 @@
 package emschenn.csie.ncku.webparsetest;
 
 import android.content.Intent;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class Teach extends AppCompatActivity {
     private Toolbar toolbar;
+    ViewPager viewPager;
+    LinearLayout slideDots;
+    ViewPagerAdapter viewPagerAdapter;
+    private ImageView[] dots;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,5 +37,40 @@ public class Teach extends AppCompatActivity {
             }
         });
 
+        viewPager = findViewById(R.id.view_page);
+        slideDots = findViewById(R.id.slide_dots);
+
+        viewPagerAdapter = new ViewPagerAdapter(this);
+        viewPager.setAdapter(viewPagerAdapter);
+
+        dots = new  ImageView[viewPagerAdapter.getCount()];
+        for (int i = 0; i < viewPagerAdapter.getCount(); i++) {
+            dots[i] = new ImageView(this);
+            dots[i].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.nonactive_dot));
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+            params.setMargins(8,0,8,0);
+            slideDots.addView(dots[i],params);
+        }
+        dots[0].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.active_dot));
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                for (int i = 0; i < viewPagerAdapter.getCount(); i++) {
+                    dots[i].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.nonactive_dot));
+                }
+                dots[position].setImageDrawable(ContextCompat.getDrawable(getApplicationContext(), R.drawable.active_dot));
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
     }
 }
